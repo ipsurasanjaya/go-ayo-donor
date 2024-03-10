33446e6bab6663705ca0609064289cacbc8dc50c
ipsurasanjaya <surasanjaya@gmail.com>
2024-03-10 15:52:07 +0800

    feat: delete unused migrations

--- db/migrations/20240309172715_create_regencies_table.down.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS regencies;
-ALTER TABLE "regencies" DROP CONSTRAINT fk_province_id;
--- db/migrations/20240309172715_create_regencies_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE regencies (
-    id bigserial PRIMARY KEY,
-    province_id bigint,
-    name text, 
-    udd_code varchar
-);
-
-ALTER TABLE regencies ADD CONSTRAINT fk_province_id FOREIGN KEY ("province_id") REFERENCES "provinces" ("id");

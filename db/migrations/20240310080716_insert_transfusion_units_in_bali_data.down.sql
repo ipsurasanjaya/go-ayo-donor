77fb75e88f1c929f6bbd53fc2e2aad36e4e1cc1e
ipsurasanjaya <surasanjaya@gmail.com>
2024-03-10 16:55:57 +0800

    feat: add migration to insert transfusions unit in bali

---
+++ db/migrations/20240310080716_insert_transfusion_units_in_bali_data.down.sql
@@ -0,0 +1,1 @@
+DELETE FROM transfusion_units WHERE province_id = 2;
---
+++ db/migrations/20240310080716_insert_transfusion_units_in_bali_data.up.sql
@@ -0,0 +1,11 @@
+INSERT INTO transfusion_units (name, code, province_id) VALUES 
+('UTD PMI Provinsi Bali', '5171', 2),
+('UTD PMI Kabupaten Buleleng', '5108', 2),
+('UTD PMI Kota Denpasar', '5172', 2),
+('UTD PMI Kabupaten Badung', '5103', 2),
+('UTD PMI Kabupaten Tabanan', '5102', 2),
+('UTD PMI Kabupaten Jembrana', '5101', 2),
+('UTD PMI Kabupaten Klungkung', '5105', 2),
+('UTD PMI Kabupaten Karangasem', '5107', 2),
+('UTD PMI Kabupaten Gianyar', '5104', 2),
+('UTD PMI Kabupaten Bangli', '5106', 2);
